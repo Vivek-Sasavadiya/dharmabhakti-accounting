3781e3765f5f8a2af9da427db5bdb2f0b9b9ce7c
Vivek Sasavadiya <vivek@example.com>
2025-10-07 02:21:59 +0530

    Remove unused _redirects.sh

--- _redirects.sh
+++
@@ -1,6 +0,0 @@
-/income-tax    /income-tax.html   200
-/gst           /gst.html          200
-/about         /about.html        200
-/services      /services.html     200
-/blog          /blog.html         200
-/contact       /contact.html      200
