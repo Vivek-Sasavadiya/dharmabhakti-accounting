b2fa6a3e59430541d8109d91075fab112bcb6b71
Vivek Sasavadiya <vivek@example.com>
2025-10-07 02:12:53 +0530

    Initial commit

---
+++ _redirects.sh
@@ -0,0 +1,6 @@
+/income-tax    /income-tax.html   200
+/gst           /gst.html          200
+/about         /about.html        200
+/services      /services.html     200
+/blog          /blog.html         200
+/contact       /contact.html      200
